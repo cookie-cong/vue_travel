f66ab6936d07638ae249e5ad3aa2f2b1b3bc3d74
蜡笔小柯南 <czy835388565@163.com>
2019-02-15 01:31:20 +0800

    2-15-2

--- src/pages/City/components/Search.vue
+++ src/pages/City/components/Search.vue
@@ -1,11 +1,19 @@
 <template>
-    <div class="CitySearch">
-        <input class="search-input" type="text" placeholder="输入城市名或拼音"/>
+    <div>
+        <div class="CitySearch">
+            <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音"/>
+        </div>
+        <div class="search-content">123</div>
     </div>
 </template>
 <script>
 export default {
-    name:"CitySearch"
+    name:"CitySearch",
+    data(){
+        return{
+            keyword:""
+        }
+    }
 }
 </script>
 <style lang="stylus" scoped>
@@ -23,6 +31,15 @@ export default {
             text-align:center
             border-radius :.06rem
             color:#666
+    .search-content
+        overflow: hidden 
+        z-index:1
+        position:absolute
+        top:1.58rem
+        left:0
+        right:0
+        bottom:0
+        background:pink
             
             
 </style>
